ddff472f732778631c90a5b3d17b31261bce57df
Yi <bboyyi@live.cn>
2021-04-07 15:08:23 +0800

    the place dream start

---
+++ src/main/java/MainController.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class MainController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!爱之梦开始的地方";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MainController.class, args);
+    }
+}
